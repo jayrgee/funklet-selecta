05980926dc2afe5653b43ac1b148aa5fb981e5c7
John Gantner <231877+jayrgee@users.noreply.github.com>
2021-06-20 23:54:16 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const groupBy = (key) => (array) =>
+  array.reduce(
+    (objectsByKeyValue, obj) => ({
+      ...objectsByKeyValue,
+      [obj[key]]: (objectsByKeyValue[obj[key]] || []).concat(obj)
+    }),
+    {}
+  );
+
+const groupByDrummer = groupBy("drummer");
+
+window.beats = [];
+
+((beats) => {
+  fetch("data/beats.json")
+    .then((response) => response.json())
+    .then((data) => {
+      //beats = data;
+      beats = data.sort((a, b) => {
+        const textA = a.drummer.toUpperCase();
+        const textB = b.drummer.toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
+      //window.beats = beats;
+
+      const defaultBeat = {
+        id: "",
+        drummer: "It's the Funklet!"
+      };
+
+      const beatsByDrummer = groupByDrummer(beats);
+      // console.log(beatsByDrummer);
+
+      // get select element
+      const sel = document.getElementById("beat_1");
+      for (const drummer in beatsByDrummer) {
+        // create opt group
+        const grp = document.createElement("OPTGROUP");
+        grp.label = drummer;
+
+        beatsByDrummer[drummer].forEach((b) => {
+          // append option
+          const opt = document.createElement("OPTION");
+          opt.textContent = b.title;
+          opt.value = b.id;
+          grp.append(opt);
+        });
+        // append opt group
+        sel.append(grp);
+      }
+      sel.addEventListener("change", function () {
+        const funkletUrl = "https://funklet.com/";
+        const baseUrl = "https://machine.funklet.com/funklet.html";
+        if (this.value) {
+          const id = this.value;
+          console.log("You selected: ", id);
+
+          const find = beats.find((b) => b.id === id);
+
+          document.getElementById("drummer_1").innerText = find.drummer
+            ? find.drummer
+            : defaultBeat.drummer;
+
+          document.getElementById("machine_1").src = find.queryString
+            ? [baseUrl, "?", find.queryString].join("")
+            : baseUrl;
+
+          document.getElementById("funklet_1").href = find.id
+            ? [funkletUrl, find.id].join("")
+            : funkletUrl;
+        } else {
+          document.getElementById("drummer_1").innerText = defaultBeat.drummer;
+          document.getElementById("machine_1").src = baseUrl;
+          document.getElementById("funklet_1").href = funkletUrl;
+        }
+      });
+    });
+})(window.beats);
